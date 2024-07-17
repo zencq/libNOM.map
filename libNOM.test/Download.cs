bde1d3c493618e3cf70128106c2999f9b71e1bbd
Christian Engelhardt <cengelha.appdevl@outlook.com>
2024-07-17 12:14:12 +0200

    test(Download): create download directory before fallback file

--- libNOM.test/Download.cs
+++ libNOM.test/Download.cs
@@ -14,10 +14,10 @@ public class DownloadTest
         // Arrange
         var content = Properties.Resources.mapping_46504_download; // version altered to 9.99.0.9
         var initialVersion = Mapping.Version;
-        var path = "download/mapping.json";
 
         // Act
-        File.WriteAllBytes(path, content); // fake existing file to test workflow independent of download result
+        Directory.CreateDirectory("download");
+        File.WriteAllBytes("download/mapping.json", content); // fake existing file as fallback to test workflow even if download itself fails
         Mapping.Update();
 
         // Assert
